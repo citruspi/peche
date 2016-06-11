bc6c9e6907548ceb1556cbde0bda6cdfce386d8d
Mihir Singh <git.service@mihirsingh.com>
2016-06-10 20:31:58 -0400

    Added setup, flush, and close methods to the base Handler class

--- peche/logging/handlers/handler.py
+++ peche/logging/handlers/handler.py
@@ -3,10 +3,20 @@
 
 from peche.logging import Level
 
+
 class Handler(object):
 
     levels = [Level.Debug, Level.Info, Level.Warn, Level.Error,
               Level.Critical]
 
+    def setup(self, event):
+        pass
+
     def on_event(self, event):
         pass
+
+    def flush(self):
+        pass
+
+    def close(self):
+        self.flush()
