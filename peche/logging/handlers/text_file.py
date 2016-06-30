2cfdbf912debca3c3efbf273b38b7f8d0b527507
Mihir Singh <git.service@mihirsingh.com>
2016-06-29 23:11:27 -0400

    Implemented setup(), flush(), and close() for the text file handler

--- peche/logging/handlers/text_file.py
+++ peche/logging/handlers/text_file.py
@@ -5,14 +5,40 @@ from peche.logging.handlers import Stdout
 
 
 class TextFileHandler(Stdout):
-    def __init__(self, path=None, name='peche'):
+
+    file_handle = None
+
+    def __init__(self, path=None, name='peche', file_handle=None):
         super().__init__()
 
-        self.path = path or '/var/log/{name}.log'.format(name=name)
+        self.path = file_handle.name if file_handle else path or '/var/log/{name}.log'.format(name=name)
+        self.file_handle = file_handle
+
+    def setup(self):
+        if self.file_handle is None:
+            try:
+                self.file_handle = self.open_handle()
+            except IOError:
+                raise
+
+    def open_file_handle(self):
+        self.file_handle = open(self.path, 'a')
 
     def format(self, template, event):
-        return '{e}\n'.format(e=super().format(template, event))
+        return '{}\n'.format(super().format(template, event))
+
+    def flush(self):
+        self.handle.flush()
+
+    def close(self):
+        super().close()
+        self.file_handle.close()
 
     def on_event(self, event):
-        with open(self.path, 'w') as f:
-            f.write(self.format(self.template(event), event))
+        formatted = self.format(self.template(event), event)
+
+        try:
+            self.handle.write(formatted)
+        except ValueError:
+            self.open_file_handle()
+            self.file_handle.write(formatted)
